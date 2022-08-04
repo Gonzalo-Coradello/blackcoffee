37977677194b60882c89bc610b83a20febb57c16
Gonzalo Coradello <gonzalocoradello@gmail.com>
2022-08-04 09:01:44 -0300

    se utilizaron funciones de orden superior

--- js/main.js
+++ js/main.js
@@ -3,27 +3,29 @@
 // Funcionalidades:
 // ○ Añadir productos al carrito, sumar sus valores y devolver el total.
 // ○ Hacer un descuento del 15% si se introduce el cupón correcto.
-// ○ Calcular el precio del envío según la distancia y sumarlo al valor total.
+// ○ Calcular el precio del envío según la distancia y sumarlo al valor total. El envío no tiene costo si la compra supera los $5000.
 
 // =========================================================================== //
 
-// Clase constructora
+// Arrays
+const productos = [];
+const carrito = [];
+
+// Clase constructora + métodos de objetos
+// En el método agregarAlCarrito se utilizan dos funciones de orden superior, reduce y map
 class Producto {
-    constructor (nombre, tipo, origen, info, precio) {
+    constructor (nombre, tipo, info, precio) {
         this.nombre = nombre
         this.tipo = tipo;
-        this.origen = origen;
         this.info = info;
         this.precio = precio;
     }
 
-    sumar () {
-        return precioTotal += this.precio;
-    }
-
     agregarAlCarrito () {
-        carrito.push([this.nombre, this.precio]); 
-        this.sumar()
+        carrito.push(this); 
+        precioTotal = carrito.reduce((acc, el) => acc + el.precio, 0);
+        listaProductos = carrito.map ((el) => `${el.nombre}: $${el.precio}`);
+        listaProductos.push(`Precio total: $${precioTotal}`);
     }
 
     mostrarInfo () {
@@ -31,10 +33,28 @@ class Producto {
     }
 }
 
+// Productos (objects) y variables
+
+productos.push(new Producto ("Café Brasil", "café", "Café intenso con notas de cacao amargo y frutos secos como avellana. Acidez media con retrogusto persistente y gran cuerpo.", 1600));
+productos.push(new Producto ("Café Colombia", "café", "Café de intensidad media con notas frutales, acidez media alta cítrica y brillante, y cuerpo medio.", 2050));
+productos.push(new Producto ("Café Perú", "café", "Café de intensidad media con notas a cacao, ciruela y leves notas cítricas. Acidez media alta.", 2200));
+productos.push(new Producto ("Café Etiopía", "café", "Café de baja intensidad, acidez y cuerpo medio. Aromas florales con notas de pasas de uva, frutos secos, mermelada y jazmín.", 2520));
+
+let info = "";
+productos[0].mostrarInfo();
+productos[1].mostrarInfo();
+productos[2].mostrarInfo();
+productos[3].mostrarInfo();
+
+let precioTotal = 0;
+let entrada;
+let listaProductos;
+
+let envioBase = 400;
+let precioEnvio = 0;
+
 // Funciones
-function mostrarMenu() {
-    entrada = prompt ("Elija su producto \n 1 - Café de Brasil ($1600) \n 2 - Café de Colombia ($2050) \n 3 - Café de Perú ($2200) \n 4 - Café de Etiopía ($2520) \n 5 - Mostrar información \n 6 - Finalizar la compra");
-}
+let mostrarMenu = () => entrada = prompt ("Elija su producto \n 1 - Café de Brasil ($1600) \n 2 - Café de Colombia ($2050) \n 3 - Café de Perú ($2200) \n 4 - Café de Etiopía ($2520) \n 5 - Mostrar información \n 6 - Finalizar la compra");
 
 function cuponDescuento() {
     let ingresarCupon = prompt("Ingrese el cupón de descuento\n(DESC15)")
@@ -48,8 +68,7 @@ function cuponDescuento() {
 }
 
 function calcularPrecioEnvio (){
-    let provincia = prompt("Ingrese su provincia");
-    provincia = provincia.toLowerCase();
+    let provincia = prompt("Ingrese su provincia").toLowerCase();
 
     switch (provincia) {
         case "buenos aires":
@@ -101,58 +120,29 @@ function calcularPrecioEnvio (){
     }
 }
 
-// Productos (objects) y variables
-
-const cafeBrasil = new Producto ("Café Brasil", "café", "Brasil", "Café intenso con notas de cacao amargo y frutos secos como avellana. Acidez media con retrogusto persistente y gran cuerpo.", 1600);
-const cafeColombia = new Producto ("Café Colombia", "café", "Colombia", "Café de intensidad media con notas frutales, acidez media alta cítrica y brillante, y cuerpo medio.", 2050)
-const cafePeru = new Producto ("Café Perú", "café", "Perú", "Café de intensidad media con notas a cacao, ciruela y leves notas cítricas. Acidez media alta.", 2200)
-const cafeEtiopia = new Producto ("Café Etiopía", "café", "Etiopía", "Café de baja intensidad, acidez y cuerpo medio. Aromas florales con notas de pasas de uva, frutos secos, mermelada y jazmín.", 2520)
-
-let info = "";
-cafeBrasil.mostrarInfo();
-cafeColombia.mostrarInfo();
-cafePeru.mostrarInfo();
-cafeEtiopia.mostrarInfo();
-
-let precioTotal = 0;
-let entrada;
-
-let envioBase = 400;
-let precioEnvio = 0;
-
-// Arrays
-
-const productos = [];
-productos.push(cafeBrasil);
-productos.push(cafeColombia);
-productos.push(cafePeru);
-productos.push(cafeEtiopia);
-
-const carrito = [];
-
-
 
 // Menú principal (agregar productos al carrito o ver información)
 do {mostrarMenu();
     switch (entrada) {
         case "1":
-            cafeBrasil.agregarAlCarrito();
+            productos[0].agregarAlCarrito();
             break;
         case "2":
-            cafeColombia.agregarAlCarrito();
+            productos[1].agregarAlCarrito();
             break;
         case "3":
-            cafePeru.agregarAlCarrito();
+            productos[2].agregarAlCarrito();
             break;
         case "4":
-            cafeEtiopia.agregarAlCarrito();
+            productos[3].agregarAlCarrito();
             break;
         case "5":
             alert(info);
             break;
         case "6":
             if (precioTotal != 0) {
-                let confirmarCompra = prompt(`${carrito.join("\n")}\n\nPrecio total: $${precioTotal}\n\n1 - Confirmar compra \n2 - Tengo un cupón de descuento`);   
+                let confirmarCompra = prompt(listaProductos.join("\n") + "\n\n1 - Confirmar compra \n2 - Tengo un cupón de descuento");
+                
                 if (confirmarCompra == "1"){
                 calcularPrecioEnvio();
                 }
